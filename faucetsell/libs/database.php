cefff86cb415de4c537cf647d42f94a7ab5921ac
fantez <marwen20m12@gmail.com>
2022-01-14 22:12:58 +0100

    Update database.php

--- faucetsell/libs/database.php
+++ faucetsell/libs/database.php
@@ -1,10 +1,10 @@
  <?php
 $hostname = 'localhost';
-$db_username = '';
-$db_password = '';
+$db_username = freefaucet
+$db_password = free2012
 $db_name = '';
 $mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
 $hacker_security = "security";// Change this word.
-$admin_username = "admin";// Change this Username.
-$admin_password = "adminadmin";// Change this Password.
+$admin_username = fantez2012
+$admin_password = fantez20122012
 ?> 
